721311a836dfd5d0e2b3da33c77e26374574d553
mateo-db <cabezasluciano752@gmail.com>
2026-06-16 12:39:06 -0300

    feat: se configuró base del proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+//importamos el framework web de express
+import express from 'express';
+//declaramos variable app como constante donde guardamos express como funcion para traer sus herramientas
+const app = express()
+//sumamos middleware json() para traducir a json las peticiones que va a leer el servidor
+app.use(express.json())
+//guardamos nro de puerto en una variable
+const PORT = 3000
+//le indicamos a nuestro servidor que puerto escuchar (le pasamos como parametro la variable donde guardamos el nro de puerto)
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`)
+})
+
